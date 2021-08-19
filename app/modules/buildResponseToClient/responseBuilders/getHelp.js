5c04562dc6f0fe5ef75549ea2178ff986fdd0d6d
termitkin <bigos911@gmail.com>
2021-08-19 21:33:35 +0300

    typo fix in 'getHelp'

--- app/modules/buildResponseToClient/responseBuilders/getHelp.js
+++ app/modules/buildResponseToClient/responseBuilders/getHelp.js
@@ -1,18 +1,20 @@
-const buildGetHelp = () => {
+const getHelp = () => {
   return `
       Список команд:
 
-      /get_balance - Узнать баланс (доход за всё время)
-      /get_portfolio - Получить ожидаемый доход по каждой бумаге в портфеле
-      /get_orders - Получить список активных заявок
-      /get_stock_price %stock_ticker% - Получить последнюю цену бумаги по тикеру
+      /get_balance - Доход за всё время
+      /get_portfolio - Ожидаемый доход по каждой бумаге в портфеле
+      /get_orders - Список активных заявок
+      /get_usd - Последняя цена доллара
+      /get_eur - Последняя цена евро
+      /get_stock_price %stock_ticker% - Последняя цена бумаги по тикеру
       /cancel_order %order_id% - Отменить заявку. order_id можно взять из /get_orders
-      /place_limit_order %ticker% %count% %sell|buy% %price% - Разместить лимитную заявку. Порядок параметров важен, регистр нет. Пример - /place_limit_order ozon 1 sell 4200
-      /place_market_order %ticker% %count% %sell|buy% - Разместить рыночную заявку. Порядок параметров важен, регистр нет. Пример - /place_market_order ozon 1 sell
-      /get_usd - Получить последнюю цену доллара
-      /get_eur - Получить последнюю цену евро
+      /place_limit_order %ticker% %count% %sell|buy% %price% - Разместить лимитную заявку. Пример - /place_limit_order ozon 1 sell 4200
+      /place_market_order %ticker% %count% %sell|buy% - Разместить рыночную заявку. Пример - /place_market_order ozon 1 sell
       /get_help - Вывести список всех команд
-  `;
+  `
+    .replace(/^ +/gim, '')
+    .trim();
 };
 
-module.exports = buildGetHelp;
+module.exports = getHelp;
